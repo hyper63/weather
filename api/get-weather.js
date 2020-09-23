161dac068b7ca80f43672b41fdf0b62baa50ed88
Tom Wilson <twilson63@gmail.com>
2020-09-23 13:57:26 -0400

    update weather base

--- api/get-weather.js
+++ api/get-weather.js
@@ -11,6 +11,7 @@ module.exports = (req, res) => {
 	const qs = {...defaults, city: req.query.city, country: req.query.country }
   fetch(`https://api.weatherbit.io/v2.0/current?city=${qs.city}&country=${qs.country}&key=${API_KEY}`)
 	  .then(res => res.json())
+		.then(result => result.count === 1 ? result.data[0] : {message: 'no data'} )
     .then(res.send.bind(res))
 
 	//res.send({ temp: '74' })
